d11a4444d7189241621249a1c623cdeefc941881
angelqva <angel.napolesqva@gmail.com>
2024-06-30 17:19:23 -0400

    add: ignore update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(9i*32xjmc@chjvx!rlhv5xbiw@-es(p+wza*xa%0)dd0fpcf^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
