4fd4935c8707b477155f70c000f389545ceec5d5
angelqva <angel.napolesqva@gmail.com>
2024-07-14 15:44:27 -0400

    change access to one day

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -178,7 +178,7 @@ SWAGGER_SETTINGS = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=3600),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
